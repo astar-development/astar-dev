e204cc8cf16112829226ec732d55acb6df034b38
Jason Barden <jason.barden@outlook.com>
2025-06-25 23:04:20 +0100

    Add the NuGet packages test projects

---
+++ test/nuget-packages/AStar.Dev.Infrastructure.FilesDb.Tests.Unit/EnumerableExtensionsShould.cs
@@ -0,0 +1,82 @@
+﻿using AStar.Dev.Infrastructure.FilesDb.Data;
+using AStar.Dev.Infrastructure.FilesDb.Fixtures;
+using AStar.Dev.Infrastructure.FilesDb.Models;
+
+namespace AStar.Dev.Infrastructure.FilesDb;
+
+public sealed class EnumerableExtensionsShould(FilesContextFixture filesContextFixture) : IClassFixture<FilesContextFixture>
+{
+    private readonly FilesContext sut = filesContextFixture.Sut;
+
+    [Fact(Skip = "The underlying code is broken")]
+    public void ReturnCorrectCountWhenFilteringImages()
+    {
+        var response = sut.Files.FilterImagesIfApplicable("Images", CancellationToken.None);
+
+        response.ToString()!.ShouldMatchApproved();
+    }
+
+    [Fact(Skip = "The underlying code is broken")]
+    public void ReturnCorrectCountWhenAllFileTypesSpecified()
+    {
+        var response = sut.Files.FilterImagesIfApplicable("AllFiles", CancellationToken.None);
+
+        response.ToString()!.ShouldMatchApproved();
+    }
+
+    [Fact(Skip = "The underlying code is broken")]
+    public void ReturnCorrectCountWhenDuplicatesSpecified()
+    {
+        var response = sut.Files.FilterImagesIfApplicable("Duplicates", CancellationToken.None);
+
+        response.ToString()!.ShouldMatchApproved();
+    }
+
+    [Fact(Skip = "The underlying code is broken")]
+    public void ReturnTheExpectedFilesListByNameAscending()
+    {
+        var response = sut.Files.OrderFiles(SortOrder.NameAscending);
+
+        response.ToString()!.ShouldMatchApproved();
+    }
+
+    [Fact(Skip = "The underlying code is broken")]
+    public void ReturnTheExpectedFilesListByNameDescending()
+    {
+        var response = sut.Files.OrderFiles(SortOrder.NameDescending);
+
+        response.ToString()!.ShouldMatchApproved();
+    }
+
+    [Fact(Skip = "The underlying code is broken")]
+    public void ReturnTheExpectedFilesListBySizeAscending()
+    {
+        var response = sut.Files.OrderFiles(SortOrder.SizeAscending);
+
+        response.ToString()!.ShouldMatchApproved();
+    }
+
+    [Fact(Skip = "The underlying code is broken")]
+    public void ReturnTheExpectedFilesListBySizeDescending()
+    {
+        var response = sut.Files.OrderFiles(SortOrder.SizeDescending);
+
+        response.ToString()!.ShouldMatchApproved();
+    }
+
+    [Fact(Skip = "The underlying code is broken")]
+    public void ReturnTheCorrectDuplicatesCount()
+    {
+        var response = sut.Files.GetDuplicatesCount(CancellationToken.None);
+
+        response.ShouldBe(30);
+    }
+
+    [Fact(Skip = "The underlying code is broken")]
+    public void ReturnTheCorrectDuplicates()
+    {
+        var response = sut.Files.GetDuplicates();
+
+        response.ToString()!.ShouldMatchApproved();
+    }
+}
