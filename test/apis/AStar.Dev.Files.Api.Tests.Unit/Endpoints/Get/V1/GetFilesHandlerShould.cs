93db7012398cb86859678f59f9ea21f322abbc6a
Jason Barden <jason.barden@outlook.com>
2025-07-19 22:19:47 +0100

    Moving to use Sqlite as the database provider in the tests

--- test/apis/AStar.Dev.Files.Api.Tests.Unit/Endpoints/Get/V1/GetFilesHandlerShould.cs
+++ test/apis/AStar.Dev.Files.Api.Tests.Unit/Endpoints/Get/V1/GetFilesHandlerShould.cs
@@ -1,32 +1,26 @@
-using AStar.Dev.Infrastructure.FilesDb.Data;
+using DbContextHelpers.Fixtures;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
 using NSubstitute;
 
 namespace AStar.Dev.Files.Api.Endpoints.Get.V1;
 
-public class GetFilesHandlerShould
+public class GetFilesHandlerShould : IClassFixture<FilesContextFixture>
 {
-    private readonly TimeProvider mockTimeProvider;
+    private readonly FilesContextFixture mockFilesContextFactory;
+    private readonly TimeProvider        mockTimeProvider;
 
-    public GetFilesHandlerShould()
+    public GetFilesHandlerShould(FilesContextFixture mockFilesContextFactory)
     {
-        mockTimeProvider = Substitute.For<TimeProvider>();
+        this.mockFilesContextFactory = mockFilesContextFactory;
+        mockTimeProvider             = Substitute.For<TimeProvider>();
         mockTimeProvider.GetUtcNow().Returns(new DateTimeOffset(new (2025, 7, 13, 1, 2, 3, DateTimeKind.Utc)));
     }
 
-    [Fact(Skip = "Once happy with the build running test, come back to this")]
+    [Fact]
     public async Task ReturnBadRequestWhenDuplicatesAreRequested()
     {
-        var connection = new SqliteConnection("Filename=:memory:");
-        await connection.OpenAsync();
-        var optionsBuilder = new DbContextOptionsBuilder<FilesContext>();
-        optionsBuilder.UseSqlServer(connection);
-        var mockContext = new FilesContext(optionsBuilder.Options);
-        await mockContext.Database.EnsureCreatedAsync();
-
-        var sut = new GetFilesHandler();
+        await using var mockContext = mockFilesContextFactory.Sut;
+        var             sut         = new GetFilesHandler();
 
         var response = await sut.HandleAsync(new (), mockContext, mockTimeProvider, "Test User", CancellationToken.None);
 
