0f00ee2c4722a9e7478f0524b474baa7f91ec3ba
Jason Barden <jason.barden@outlook.com>
2025-08-17 23:15:44 +0100

    Add tests for the AStar.Dev.Database.Updater service
    Next, will add tests for the Core project

--- src/services/astar-dev-database-updater/AStar.Dev.Database.Updater/Program.cs
+++ src/services/astar-dev-database-updater/AStar.Dev.Database.Updater/Program.cs
@@ -1,13 +1,5 @@
 using System.Diagnostics;
-using System.IO.Abstractions;
-using System.Text.Encodings.Web;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using AStar.Dev.Database.Updater;
-using AStar.Dev.Database.Updater.Core;
-using AStar.Dev.Logging.Extensions;
-using AStar.Dev.ServiceDefaults;
-using Microsoft.Extensions.Options;
 using Serilog;
 using ILogger = Serilog.ILogger;
 
@@ -20,33 +12,7 @@ try
     logger     = Log.Logger;
     var builder = Host.CreateApplicationBuilder(args);
 
-    _ = builder.Services.Configure<JsonSerializerOptions>(options =>
-                                                          {
-                                                              options.PropertyNamingPolicy        = JsonNamingPolicy.CamelCase;
-                                                              options.DefaultIgnoreCondition      = JsonIgnoreCondition.WhenWritingNull;
-                                                              options.WriteIndented               = true;
-                                                              options.Encoder                     = JavaScriptEncoder.Default;
-                                                              options.AllowTrailingCommas         = true;
-                                                              options.NumberHandling              = JsonNumberHandling.AllowReadingFromString;
-                                                              options.PropertyNameCaseInsensitive = true;
-                                                              options.ReferenceHandler            = ReferenceHandler.IgnoreCycles;
-                                                          });
-
-    _ = builder.AddServiceDefaults();
-    _ = builder.Configuration.AddJsonFile(Configuration.ExternalSettingsFile, false, true);
-
-    _ = builder.Services
-               .AddOptions<DatabaseUpdaterConfiguration>()
-               .Bind(builder.Configuration.GetSection(DatabaseUpdaterConfiguration.ConfigurationSectionName));
-
-    _ = builder.Services.AddSerilog((sp, loggerConfig) => loggerConfig.ReadFrom.Configuration(sp.CreateScope().ServiceProvider.GetRequiredService<IConfiguration>()));
-
-    _ = builder.Services.AddSingleton<AddNewFilesService>();
-    _ = builder.Services.AddSingleton<IValidateOptions<DatabaseUpdaterConfiguration>, DatabaseUpdaterConfigurationValidator>();
-    _ = builder.Services.AddSingleton<TimeDelay>();
-    _ = builder.Services.AddSingleton<IFileSystem, FileSystem>();
-
-    _ = builder.Services.AddHostedService<NewFilesBackgroundService>();
+    _ = builder.ConfigureApplicationServices();
 
     var host = builder.Build();
 
