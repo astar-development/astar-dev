66c6e1733c5bbd7cd317b4da27a4184524ee4e89
Jason Barden <jason.barden@outlook.com>
2025-10-11 21:10:01 +0100

    Add example from FluentUI and start on Dup Images

--- src/uis/AStar.Dev.Web/Components/Pages/Images/DuplicateImages.razor.cs
+++ src/uis/AStar.Dev.Web/Components/Pages/Images/DuplicateImages.razor.cs
@@ -1,7 +1,18 @@
+using AStar.Dev.Web.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace AStar.Dev.Web.Components.Pages.Images;
 
 public partial class DuplicateImages : ComponentBase
 {
+    private readonly JustifyContent Justification = JustifyContent.FlexStart;
+    private readonly int            Spacing       = 3;
+
+    [SupplyParameterFromForm]
+    private SearchModel SearchModel { get; } = new();
+
+    private void HandleValidSubmit()
+    {
+    }
 }
---
+++ src/uis/AStar.Dev.Web/Models/SearchModel.cs
@@ -0,0 +1,9 @@
+namespace AStar.Dev.Web.Models;
+
+public class SearchModel
+{
+    public string? SearchString { get; set; }
+
+    public string StartingDirectory { get; set; } = "/media/jason-barden/ONEDRIVE/Pictures/_lookat";
+    public string SearchType        { get; set; } = "";
+}
