babff424161bedecb797cad5e925a0cccafe657c
Jason Barden <jason.barden@outlook.com>
2025-10-16 23:48:17 +0100

    Bring most NuGet package projects back

---
+++ src/nuget-packages/AStar.Dev.Api.Usage.Sdk/Metrics/UsageMetricHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace AStar.Dev.Api.Usage.Sdk.Metrics;
+
+/// <summary>
+/// </summary>
+/// <param name="next"></param>
+/// <param name="send"></param>
+public sealed class UsageMetricHandler(RequestDelegate next, Send send)
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="context"></param>
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var request = context.Request;
+        var httpMethod = request.Method;
+        string apiEndpoint = request.Path;
+        var apiName = request.Host.Host;
+
+        apiName = UpdateApiNameIfRequired(apiName);
+
+        var startTimestamp = Stopwatch.GetTimestamp();
+
+        await next(context);
+
+        var endTimestamp = Stopwatch.GetTimestamp();
+        var diff = Stopwatch.GetElapsedTime(startTimestamp, endTimestamp);
+
+        await send.SendUsageEventAsync(new(apiName, apiEndpoint, httpMethod, diff.TotalMilliseconds, context.Response.StatusCode), CancellationToken.None);
+    }
+
+    private static string UpdateApiNameIfRequired(string apiName)
+    {
+        if(apiName == "host.docker.internal")
+        {
+            apiName = "astar.dev.images.api";
+        }
+
+        return apiName;
+    }
+}
---
+++ test/nuget-packages/AStar.Dev.Infrastructure.FilesDb.Tests.Unit/Models/FileDetailShould.cs
@@ -0,0 +1,12 @@
+﻿namespace AStar.Dev.Infrastructure.FilesDb.Models;
+
+public sealed class FileDetailShould
+{
+    [Fact]
+    public void ReturnTheExpectedToStringRepresentation()
+    {
+        var fileDetail = new FileDetail { FileName = new("test.txt"), DirectoryName = new("C:\\") };
+
+        fileDetail.ToString().ShouldMatchApproved();
+    }
+}
