988a18a97c665ec3ac59b853dd30d43e1354664f
Jason Barden <jason.barden1@outlook.com>
2025-10-27 13:56:10 +0000

    Add missed files

---
+++ src/nuget-packages/AStar.Dev.Infrastructure.FilesDb/Migrations/20251027134318_AddNullableParentIdOnFileClassification.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AStar.Dev.Infrastructure.FilesDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNullableParentIdOnFileClassification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                schema: "files",
+                table: "FileClassification",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                schema: "files",
+                table: "FileClassification");
+        }
+    }
+}
