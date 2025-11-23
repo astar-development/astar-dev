3bcf2ec5923d9d5a049cc05db2d2924fd11e57bd
Jason <jason.barden1@outlook.com>
2025-11-23 21:41:20 +0000

    Add the AStar.Dev.Functional.Extensions project and associated tests

---
+++ test/nuget-packages/AStar.Dev.Functional.Extensions.Tests.Unit/ResultExtensionTapShould.cs
@@ -0,0 +1,236 @@
+namespace AStar.Dev.Functional.Extensions.Tests.Unit;
+
+public class ResultExtensionTapShould
+{
+    [Fact]
+    public void ExecuteSideEffectAndReturnOriginalResultWhenTappingSuccessResult()
+    {
+        var result          = new Result<int, string>.Ok(42);
+        var sideEffectValue = 0;
+
+        var tapped = result.Tap(value => sideEffectValue = value);
+
+        sideEffectValue.ShouldBe(42);
+        tapped.ShouldBeSameAs(result);
+    }
+
+    [Fact]
+    public void NotExecuteSideEffectAndReturnOriginalResultWhenTappingErrorResult()
+    {
+        var result          = new Result<int, string>.Error("error");
+        var sideEffectValue = 0;
+
+        var tapped = result.Tap(value => sideEffectValue = value);
+
+        sideEffectValue.ShouldBe(0);
+        tapped.ShouldBeSameAs(result);
+    }
+
+    [Fact]
+    public void ExecuteSideEffectAndReturnOriginalResultWhenTappingErrorOnErrorResult()
+    {
+        var result          = new Result<string, int>.Error(42);
+        var sideEffectValue = 0;
+
+        var tapped = result.TapError(value => sideEffectValue = value);
+
+        sideEffectValue.ShouldBe(42);
+        tapped.ShouldBeSameAs(result);
+    }
+
+    [Fact]
+    public void NotExecuteSideEffectAndReturnOriginalResultWhenTappingErrorOnSuccessResult()
+    {
+        var result          = new Result<string, int>.Ok("success");
+        var sideEffectValue = 0;
+
+        var tapped = result.TapError(value => sideEffectValue = value);
+
+        sideEffectValue.ShouldBe(0);
+        tapped.ShouldBeSameAs(result);
+    }
+
+    [Fact]
+    public async Task ExecuteAsyncSideEffectAndReturnOriginalResultWhenTappingSuccessResultAsync()
+    {
+        var result          = new Result<int, string>.Ok(42);
+        var sideEffectValue = 0;
+
+        var tapped = await result.TapAsync(value =>
+                                           {
+                                               sideEffectValue = value;
+
+                                               return Task.CompletedTask;
+                                           });
+
+        sideEffectValue.ShouldBe(42);
+        tapped.ShouldBeSameAs(result);
+    }
+
+    [Fact]
+    public async Task NotExecuteAsyncSideEffectAndReturnOriginalResultWhenTappingErrorResultAsync()
+    {
+        var result          = new Result<int, string>.Error("error");
+        var sideEffectValue = 0;
+
+        var tapped = await result.TapAsync(value =>
+                                           {
+                                               sideEffectValue = value;
+
+                                               return Task.CompletedTask;
+                                           });
+
+        sideEffectValue.ShouldBe(0);
+        tapped.ShouldBeSameAs(result);
+    }
+
+    [Fact]
+    public async Task ExecuteAsyncSideEffectAndReturnOriginalResultWhenTappingErrorOnErrorResult()
+    {
+        var result          = new Result<string, int>.Error(42);
+        var sideEffectValue = 0;
+
+        var tapped = await result.TapErrorAsync(value =>
+                                                {
+                                                    sideEffectValue = value;
+
+                                                    return Task.CompletedTask;
+                                                });
+
+        sideEffectValue.ShouldBe(42);
+        tapped.ShouldBeSameAs(result);
+    }
+
+    [Fact]
+    public async Task NotExecuteAsyncSideEffectAndReturnOriginalResultWhenTappingErrorOnSuccessResult()
+    {
+        var result          = new Result<string, int>.Ok("success");
+        var sideEffectValue = 0;
+
+        var tapped = await result.TapErrorAsync(value =>
+                                                {
+                                                    sideEffectValue = value;
+
+                                                    return Task.CompletedTask;
+                                                });
+
+        sideEffectValue.ShouldBe(0);
+        tapped.ShouldBeSameAs(result);
+    }
+
+    [Fact]
+    public async Task ExecuteSideEffectAndReturnOriginalResultWhenTappingSuccessTaskResultAsync()
+    {
+        var resultTask      = Task.FromResult<Result<int, string>>(new Result<int, string>.Ok(42));
+        var sideEffectValue = 0;
+
+        var tapped = await resultTask.TapAsync(value => sideEffectValue = value);
+
+        sideEffectValue.ShouldBe(42);
+        tapped.ShouldBe(await resultTask);
+    }
+
+    [Fact]
+    public async Task NotExecuteSideEffectAndReturnOriginalResultWhenTappingErrorTaskResultAsync()
+    {
+        var resultTask      = Task.FromResult<Result<int, string>>(new Result<int, string>.Error("error"));
+        var sideEffectValue = 0;
+
+        var tapped = await resultTask.TapAsync(value => sideEffectValue = value);
+
+        sideEffectValue.ShouldBe(0);
+        tapped.ShouldBe(await resultTask);
+    }
+
+    [Fact]
+    public async Task ExecuteSideEffectAndReturnOriginalResultWhenTappingErrorOnErrorTaskResult()
+    {
+        var resultTask      = Task.FromResult<Result<string, int>>(new Result<string, int>.Error(42));
+        var sideEffectValue = 0;
+
+        var tapped = await resultTask.TapErrorAsync(value => sideEffectValue = value);
+
+        sideEffectValue.ShouldBe(42);
+        tapped.ShouldBe(await resultTask);
+    }
+
+    [Fact]
+    public async Task NotExecuteSideEffectAndReturnOriginalResultWhenTappingErrorOnSuccessTaskResult()
+    {
+        var resultTask      = Task.FromResult<Result<string, int>>(new Result<string, int>.Ok("success"));
+        var sideEffectValue = 0;
+
+        var tapped = await resultTask.TapErrorAsync(value => sideEffectValue = value);
+
+        sideEffectValue.ShouldBe(0);
+        tapped.ShouldBe(await resultTask);
+    }
+
+    [Fact]
+    public async Task ExecuteAsyncSideEffectAndReturnOriginalResultWhenTappingSuccessTaskResultAsync()
+    {
+        var resultTask      = Task.FromResult<Result<int, string>>(new Result<int, string>.Ok(42));
+        var sideEffectValue = 0;
+
+        var tapped = await resultTask.TapAsync(value =>
+                                               {
+                                                   sideEffectValue = value;
+
+                                                   return Task.CompletedTask;
+                                               });
+
+        sideEffectValue.ShouldBe(42);
+        tapped.ShouldBe(await resultTask);
+    }
+
+    [Fact]
+    public async Task NotExecuteAsyncSideEffectAndReturnOriginalResultWhenTappingErrorTaskResultAsync()
+    {
+        var resultTask      = Task.FromResult<Result<int, string>>(new Result<int, string>.Error("error"));
+        var sideEffectValue = 0;
+
+        var tapped = await resultTask.TapAsync(value =>
+                                               {
+                                                   sideEffectValue = value;
+
+                                                   return Task.CompletedTask;
+                                               });
+
+        sideEffectValue.ShouldBe(0);
+        tapped.ShouldBe(await resultTask);
+    }
+
+    [Fact]
+    public async Task ExecuteAsyncSideEffectAndReturnOriginalResultWhenTappingErrorOnErrorTaskResult()
+    {
+        var resultTask      = Task.FromResult<Result<string, int>>(new Result<string, int>.Error(42));
+        var sideEffectValue = 0;
+
+        var tapped = await resultTask.TapErrorAsync(value =>
+                                                    {
+                                                        sideEffectValue = value;
+
+                                                        return Task.CompletedTask;
+                                                    });
+
+        sideEffectValue.ShouldBe(42);
+        tapped.ShouldBe(await resultTask);
+    }
+
+    [Fact]
+    public async Task NotExecuteAsyncSideEffectAndReturnOriginalResultWhenTappingErrorOnSuccessTaskResult()
+    {
+        var resultTask      = Task.FromResult<Result<string, int>>(new Result<string, int>.Ok("success"));
+        var sideEffectValue = 0;
+
+        var tapped = await resultTask.TapErrorAsync(value =>
+                                                    {
+                                                        sideEffectValue = value;
+
+                                                        return Task.CompletedTask;
+                                                    });
+
+        sideEffectValue.ShouldBe(0);
+        tapped.ShouldBe(await resultTask);
+    }
+}
