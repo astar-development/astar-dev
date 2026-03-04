ef29e1616cf4d06d64be61319c8d24bf20bc8bb9
Jason Barden <jason.barden1@outlook.com>
2026-03-04 23:24:43 +0000

    add placeholder AStar.Web blazor app and unit tests projects

---
+++ web/AStar.Dev.Web/Program.cs
@@ -0,0 +1,28 @@
+using AStar.Dev.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+  _ = app.UseExceptionHandler("/Error", createScopeForErrors: true);
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  _ = app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
--- web/ExampleApi/Program.cs
+++
@@ -1,7 +0,0 @@
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Example API running");
-
-app.Run();
