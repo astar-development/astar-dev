b894e3a601d4bd04be42b6439e4b2a53e463033a
Jason Barden <jason.barden1@outlook.com>
2026-03-12 17:06:54 +0000

    fix: add missed files

---
+++ tests/apps/AStar.Dev.OneDrive.Client.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace AStar.Dev.OneDrive.Client.Tests.Unit;
+
+/// <summary>
+/// Placeholder class - yes, this is a test class but the rules have not been updated...yet...
+/// </summary>
+public class UnitTest1
+{
+    /// <summary>
+    /// As with the class...
+    /// </summary>
+    [Fact]
+    public void Test1() => Assert.True(true);
+}
